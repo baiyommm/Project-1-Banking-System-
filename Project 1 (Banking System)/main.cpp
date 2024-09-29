2cedab645caf85baba22c9d72720c53a344dfd3e
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-09-29 03:25:41 -0700

    Added variables accountType and balance to store data. Added cout&cin to input&output user data.

--- Project 1 (Banking System)/main.cpp
+++ Project 1 (Banking System)/main.cpp
@@ -10,7 +10,14 @@ int main() {
     // code for random number generator
     srand(static_cast<unsigned int>(time(0)));
     
+    int accountType;
+    double balance;
     
+    cout << "Select Account Type: \n1. Checking Account \n2. Savings Account \n3. Business Account" << endl;
+    cin >> accountType;
+    
+    cout << "Enter Initial Balance: ";
+    cin >> balance;
     
     return 0;
 }
